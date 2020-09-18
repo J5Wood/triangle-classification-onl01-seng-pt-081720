6cc5f67299d073b64e25d0cb6c4a3658656797be
Jeremy Wood <js.wood206@gmail.com>
2020-09-18 03:53:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,11 @@ class Triangle
     @side_three = side_three
   end
 
+  def TriangleError < StandardError
+  end
+  
   def kind
+
     if side_one == side_two && side_two == side_three
       :equilateral
     elsif side_one == side_two || side_one == side_three || side_two == side_three
