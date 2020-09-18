5ae24e7bbc35acf20605824cb4fed65a8fd7d162
Jeremy Wood <js.wood206@gmail.com>
2020-09-18 03:59:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,7 +18,7 @@ class Triangle
           raise TriangleError
         rescue TriangleError => error
         end
-      
+      end
     end
     if side_one == side_two && side_two == side_three
       :equilateral
