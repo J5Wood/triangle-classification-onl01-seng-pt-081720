ff73fe27bb41e422436851cce6b7d49b3ddb7482
Jeremy Wood <js.wood206@gmail.com>
2020-09-18 03:47:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,4 +7,15 @@ class Triangle
     @side_two = side_two
     @side_three = side_three
   end
+
+  def kind
+    if side_one == side_two && side_two == side_three
+      :equilateral
+    elsif side_one == side_two || side_one == side_three || side_two == side_three
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  
 end
